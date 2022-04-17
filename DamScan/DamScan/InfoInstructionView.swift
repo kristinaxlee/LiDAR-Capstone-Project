0992b636674798eaebcb7b7e216bc91b85a6dad7
Gavin Gutowsky <gtgutowsky@gmail.com>
2022-04-16 23:36:54 -0700

    Additional button explanations

--- DamScan/DamScan/InfoInstructionView.swift
+++ DamScan/DamScan/InfoInstructionView.swift
@@ -12,9 +12,7 @@ import Foundation
 class InfoInstructionView: UIView {
     override init(frame: CGRect) {
         super.init(frame: frame)
-        setupImageView()
-        setupTextView()
-        setupInstructions()
+        setupViews()
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -111,26 +109,29 @@ class InfoInstructionView: UIView {
         return textView
     }()
     
-    func setupImageView() {
+    let uploadImage: UIImageView = {
+        let image = UIImage(systemName: "square.and.arrow.up")
+        let imageView = UIImageView(image: image!)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    let uploadText: UITextView = {
+        let textView = UITextView()
+        let attributeText = NSMutableAttributedString(string: "Tap to open a submission form including input fields for building and room scanned", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 15)])
+        textView.attributedText = attributeText
+        textView.textAlignment = .left
+        textView.isEditable = false
+        textView.isScrollEnabled = false
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        return textView
+    }()
+    
+    func setupViews() {
         self.addSubview(imageView)
-        NSLayoutConstraint.activate([
-            imageView.centerXAnchor.constraint(equalTo: self.centerXAnchor),
-            imageView.topAnchor.constraint(equalTo: self.topAnchor, constant: 30),
-            imageView.widthAnchor.constraint(equalToConstant: 150),
-            imageView.heightAnchor.constraint(equalToConstant: 175)
-        ])
-    }
-    
-    func setupTextView() {
+        
         self.addSubview(textView)
-        NSLayoutConstraint.activate([
-            textView.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 24),
-            textView.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -24),
-            textView.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 25)
-        ])
-    }
-    
-    func setupInstructions() {
+        
         self.addSubview(recordImage)
         self.addSubview(recordText)
         
@@ -142,8 +143,20 @@ class InfoInstructionView: UIView {
         
         self.addSubview(cameraImage)
         self.addSubview(cameraText)
+    
+        self.addSubview(uploadImage)
+        self.addSubview(uploadText)
         
         NSLayoutConstraint.activate([
+            imageView.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            imageView.topAnchor.constraint(equalTo: self.topAnchor, constant: 15),
+            imageView.widthAnchor.constraint(equalToConstant: 150),
+            imageView.heightAnchor.constraint(equalToConstant: 175),
+            
+            textView.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 24),
+            textView.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -24),
+            textView.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 5),
+            
             recordImage.widthAnchor.constraint(equalToConstant: 50),
             recordImage.heightAnchor.constraint(equalToConstant: 50),
             recordImage.topAnchor.constraint(equalTo: textView.bottomAnchor, constant: 5),
@@ -154,7 +167,7 @@ class InfoInstructionView: UIView {
             
             deleteImage.widthAnchor.constraint(equalToConstant: 50),
             deleteImage.heightAnchor.constraint(equalToConstant: 50),
-            deleteImage.topAnchor.constraint(equalTo: recordImage.bottomAnchor, constant: 25),
+            deleteImage.topAnchor.constraint(equalTo: recordImage.bottomAnchor, constant: 20),
             deleteImage.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 40),
             deleteText.leftAnchor.constraint(equalTo: deleteImage.rightAnchor, constant: 10),
             deleteText.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -25),
@@ -162,7 +175,7 @@ class InfoInstructionView: UIView {
             
             pointImage.widthAnchor.constraint(equalToConstant: 50),
             pointImage.heightAnchor.constraint(equalToConstant: 45),
-            pointImage.topAnchor.constraint(equalTo: deleteImage.bottomAnchor, constant: 25),
+            pointImage.topAnchor.constraint(equalTo: deleteImage.bottomAnchor, constant: 20),
             pointImage.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 40),
             pointText.leftAnchor.constraint(equalTo: pointImage.rightAnchor, constant: 10),
             pointText.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -25),
@@ -170,11 +183,19 @@ class InfoInstructionView: UIView {
             
             cameraImage.widthAnchor.constraint(equalToConstant: 50),
             cameraImage.heightAnchor.constraint(equalToConstant: 40),
-            cameraImage.topAnchor.constraint(equalTo: pointImage.bottomAnchor, constant: 25),
+            cameraImage.topAnchor.constraint(equalTo: pointImage.bottomAnchor, constant: 20),
             cameraImage.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 40),
             cameraText.leftAnchor.constraint(equalTo: cameraImage.rightAnchor, constant: 10),
             cameraText.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -25),
-            cameraText.centerYAnchor.constraint(equalTo: cameraImage.topAnchor, constant: 20)
+            cameraText.centerYAnchor.constraint(equalTo: cameraImage.topAnchor, constant: 20),
+            
+            uploadImage.widthAnchor.constraint(equalToConstant: 50),
+            uploadImage.heightAnchor.constraint(equalToConstant: 55),
+            uploadImage.topAnchor.constraint(equalTo: cameraImage.bottomAnchor, constant: 20),
+            uploadImage.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 40),
+            uploadText.leftAnchor.constraint(equalTo: uploadImage.rightAnchor, constant: 10),
+            uploadText.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -25),
+            uploadText.centerYAnchor.constraint(equalTo: uploadImage.topAnchor, constant: 25)
         ])
     }
 }
